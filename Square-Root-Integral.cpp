e1aabd79c4f3f44085da24403b20a20fcb6bb12b
Jagan Mohan Bishoyi <jagan.bishoyi@people10.com>
2022-07-19 20:12:23 +0530

    Square root calculation

---
+++ Square-Root-Integral.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (i <= n)
+    {
+        int square = i * i;
+        if (square < n)
+        {
+            i++;
+        }
+        else if (square == n)
+        {
+            break;
+        }
+        else
+        {
+            i--;
+            break;
+        }
+    }
+
+    cout << i;
+}
