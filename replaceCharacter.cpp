2a8a246f86a16e1ade3ae9607e27f1e2c6409d80
Jagan Mohan Bishoyi <jaganmohanbishoyi@gmail.com>
2022-08-01 06:35:16 +0530

    Replace string

---
+++ replaceCharacter.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void replaceCharacter(char input[], char c1, char c2)
+{
+    // Write your code here
+    int length = strlen(input) - 1;
+
+    for (int i = 0; i <= length; i++)
+    {
+        if (input[i] == c1)
+        {
+            input[i] = c2;
+        }
+    }
+}
+
+int main()
+{
+    char input[1000000];
+    char c1, c2;
+    cin >> input;
+    cin >> c1 >> c2;
+    replaceCharacter(input, c1, c2);
+    cout << input << endl;
+}
