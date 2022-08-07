f1150d22ea27dd631265fe6fe2be58b5ea169aac
Jagan Mohan Bishoyi <jaganmohanbishoyi@gmail.com>
2022-08-07 20:44:34 +0530

    Spiral Print

---
+++ spiralPrint.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+void spiralPrint(int **input, int nRows, int nCols)
+{
+    int count = 0;
+    int FR = 0;
+    int LC = nCols - 1;
+    int LR = nRows - 1;
+    int FC = 0;
+
+    while (count < (nRows * nCols))
+    {
+        // Print First row
+        for (int i = FR; i <= LC; i++)
+        {
+            cout << input[FR][i] << " ";
+            count++;
+        }
+        FR++;
+
+        // Print last Column
+        for (int i = FR; i <= LR; i++)
+        {
+            cout << input[i][LC] << " ";
+            count++;
+        }
+        LC--;
+
+        // Print Last Row
+        for (int i = LC; i >= FC; i--)
+        {
+            cout << input[LR][i] << " ";
+            count++;
+        }
+        LR--;
+
+        // Print First Column
+        for (int i = LR; i >= FR; i--)
+        {
+            cout << input[i][FC] << " ";
+            count++;
+        }
+        FC++;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int row, col;
+        cin >> row >> col;
+        int **matrix = new int *[row];
+
+        for (int i = 0; i < row; i++)
+        {
+            matrix[i] = new int[col];
+            for (int j = 0; j < col; j++)
+            {
+                cin >> matrix[i][j];
+            }
+        }
+        spiralPrint(matrix, row, col);
+
+        for (int i = 0; i < row; ++i)
+        {
+            delete[] matrix[i];
+        }
+        delete[] matrix;
+
+        cout << endl;
+    }
+}
